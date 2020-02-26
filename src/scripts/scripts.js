6f55fd280175a40b0b6a02464e853841a443ee16
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-02-26 15:05:13 +0800

    finished landing and our work

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,5 +3,10 @@ const navHamburger = document.getElementsByClassName('hamburger');
 const navContent = document.getElementsByClassName('navigation__content');
 navHamburger[0].addEventListener('click', () => {
   navHamburger[0].classList.toggle('hamburger--active');
+  navHamburger[0].setAttribute(
+    'aria-expanded',
+    navHamburger[0].getAttribute('aria-expanded') === 'false' ? 'true' : 'false',
+  );
   navContent[0].classList.toggle('navigation__content--active');
+  navContent[0].classList.remove('navigation__content--initial');
 });
