eb27a0f2f024485139db022c3811b3d6993a2668
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-03-06 22:15:14 +0800

    added calendar

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,16 +3,21 @@ import Glide from '@glidejs/glide';
 // Navigation Toggle
 const navHamburger = document.getElementsByClassName('hamburger');
 const navContent = document.getElementsByClassName('navigation__content');
-navHamburger[0].addEventListener('click', () => {
-  navHamburger[0].classList.toggle('hamburger--active');
-  navHamburger[0].setAttribute(
-    'aria-expanded',
-    navHamburger[0].getAttribute('aria-expanded') === 'false' ? 'true' : 'false',
-  );
-  navContent[0].classList.toggle('navigation__content--active');
-  navContent[0].classList.remove('navigation__content--initial');
-  document.body.classList.toggle('disable-scroll');
-});
+
+if (navHamburger[0] && navContent) {
+  navHamburger[0].addEventListener('click', () => {
+    navHamburger[0].classList.toggle('hamburger--active');
+    navHamburger[0].setAttribute(
+      'aria-expanded',
+      navHamburger[0].getAttribute('aria-expanded') === 'false'
+        ? 'true'
+        : 'false',
+    );
+    navContent[0].classList.toggle('navigation__content--active');
+    navContent[0].classList.remove('navigation__content--initial');
+    document.body.classList.toggle('disable-scroll');
+  });
+}
 
 const sprintElement = document.getElementById('processSprint');
 const aboutElement = document.getElementById('aboutTeam');
